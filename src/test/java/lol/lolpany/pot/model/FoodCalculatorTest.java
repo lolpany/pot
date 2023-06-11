a638a7b8713f1a9cd149958b1e966b4bcf32e66b
user <gbesergey@gmail.com>
2023-06-11 17:09:46 +0300

    refactor FoodCalculatorTest

--- src/test/java/lol/lolpany/pot/model/FoodCalculatorTest.java
+++ src/test/java/lol/lolpany/pot/model/FoodCalculatorTest.java
@@ -15,14 +15,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FoodCalculatorTest {
 
-    @Test
-    public void testCaloriesCalculation() throws IOException {
-        Person person = new Person(Sex.MALE, 1986, 0.17, 70, ActivityLevel.SEDENTARY);
-        FoodTarget foodTarget = new FoodTarget(WeightTarget.STAY_SAME, 1600.0, 1, 0.9, 1, 0.01, 3600, 0.9, 0.01,
-                NormalVeganVegetarian.NORMAL, 0);
-        FoodCalculator foodCalculator = new FoodCalculator();
-
-        List<Food> foods = new ArrayList<>();
+    private List<Food> readFoods() throws IOException {
+        List<Food> result = new ArrayList<>();
         Reader fileReader = new FileReader("C:\\all\\projects\\pot\\src\\test\\resources\\food14.tsv");
         Iterable<CSVRecord> records = CSVFormat.TDF.builder().setHeader().build().parse(fileReader);
         long id = 0;
@@ -33,9 +27,18 @@ public class FoodCalculatorTest {
             food.setCarbohydrates(Double.parseDouble(record.get("carbohydrates")));
             food.setPrice(Double.parseDouble(record.get("price")));
             food.setPersonalRating(Double.parseDouble(record.get("personalRating")));
-            foods.add(food);
+            result.add(food);
         }
+        return result;
+    }
 
+    @Test
+    public void testCaloriesCalculation() throws IOException {
+        Person person = new Person(Sex.MALE, 1986, 0.17, 70, ActivityLevel.SEDENTARY);
+        FoodTarget foodTarget = new FoodTarget(WeightTarget.STAY_SAME, 1600.0, 1, 0.9, 1, 0.01, 3600, 0.9, 0.01,
+                NormalVeganVegetarian.NORMAL, 0);
+        FoodCalculator foodCalculator = new FoodCalculator();
+        List<Food> foods = readFoods();
         List<FoodAndQuantity> foodsAndQuantities = foodCalculator.calculate(person, foodTarget, foods, Collections.emptySet());
         assertEquals("duck, meat only, raw", foodsAndQuantities.get(0).food.name);
         assertEquals(0.25, foodsAndQuantities.get(0).quantity);
@@ -54,19 +57,7 @@ public class FoodCalculatorTest {
                 NormalVeganVegetarian.NORMAL, 0);
         FoodCalculator foodCalculator = new FoodCalculator();
 
-        List<Food> foods = new ArrayList<>();
-        Reader fileReader = new FileReader("C:\\all\\projects\\pot\\src\\test\\resources\\food14.tsv");
-        Iterable<CSVRecord> records = CSVFormat.TDF.builder().setHeader().build().parse(fileReader);
-        long id = 0;
-        for (CSVRecord record : records) {
-            Food food = new Food(id++, record.get("name"));
-            food.setProteins(Double.parseDouble(record.get("proteins")));
-            food.setFats(Double.parseDouble(record.get("fats")));
-            food.setCarbohydrates(Double.parseDouble(record.get("carbohydrates")));
-            food.setPrice(Double.parseDouble(record.get("price")));
-            food.setPersonalRating(Double.parseDouble(record.get("personalRating")));
-            foods.add(food);
-        }
+        List<Food> foods = readFoods();
 
         List<FoodAndQuantity> foodsAndQuantities = foodCalculator.calculate(person, foodTarget, foods, Collections.emptySet());
         assertEquals("pasta, cooked", foodsAndQuantities.get(0).food.name);
@@ -84,19 +75,7 @@ public class FoodCalculatorTest {
                 NormalVeganVegetarian.NORMAL, 0);
         FoodCalculator foodCalculator = new FoodCalculator();
 
-        List<Food> foods = new ArrayList<>();
-        Reader fileReader = new FileReader("C:\\all\\projects\\pot\\src\\test\\resources\\food14.tsv");
-        Iterable<CSVRecord> records = CSVFormat.TDF.builder().setHeader().build().parse(fileReader);
-        long id = 0;
-        for (CSVRecord record : records) {
-            Food food = new Food(id++, record.get("name"));
-            food.setProteins(Double.parseDouble(record.get("proteins")));
-            food.setFats(Double.parseDouble(record.get("fats")));
-            food.setCarbohydrates(Double.parseDouble(record.get("carbohydrates")));
-            food.setPrice(Double.parseDouble(record.get("price")));
-            food.setPersonalRating(Double.parseDouble(record.get("personalRating")));
-            foods.add(food);
-        }
+        List<Food> foods = readFoods();
 
         List<FoodAndQuantity> foodsAndQuantities = foodCalculator.calculate(person, foodTarget, foods, Collections.emptySet());
         assertEquals("pasta, cooked", foodsAndQuantities.get(0).food.name);
@@ -115,24 +94,13 @@ public class FoodCalculatorTest {
                 NormalVeganVegetarian.NORMAL, 0);
         FoodCalculator foodCalculator = new FoodCalculator();
 
-        List<Food> foods = new ArrayList<>();
-        Reader fileReader = new FileReader("C:\\all\\projects\\pot\\src\\test\\resources\\food14.tsv");
-        Iterable<CSVRecord> records = CSVFormat.TDF.builder().setHeader().build().parse(fileReader);
-        long id = 0;
+        List<Food> foods = readFoods();
         long potatoId = -1;
-        for (CSVRecord record : records) {
-            Food food = new Food(id++, record.get("name"));
-            food.setProteins(Double.parseDouble(record.get("proteins")));
-            food.setFats(Double.parseDouble(record.get("fats")));
-            food.setCarbohydrates(Double.parseDouble(record.get("carbohydrates")));
-            food.setPrice(Double.parseDouble(record.get("price")));
-            food.setPersonalRating(Double.parseDouble(record.get("personalRating")));
+        for (Food food: foods) {
             if ("potato".equals(food.name)) {
                 food.id = potatoId;
             }
-            foods.add(food);
         }
-
         List<FoodAndQuantity> foodsAndQuantities = foodCalculator.calculate(person, foodTarget, foods, Collections.singleton(potatoId));
         assertEquals("pasta, cooked", foodsAndQuantities.get(0).food.name);
         assertEquals(0.25, foodsAndQuantities.get(0).quantity);
